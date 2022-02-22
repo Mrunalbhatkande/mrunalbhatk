882671acc54000895e9ab323152dfb0333b55650
Mrunalbhatkande <89386398+Mrunalbhatkande@users.noreply.github.com>
2022-02-22 15:21:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Mrunal Bhatkande</title>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+/* Style the header */
+header {
+  background-color: rgb(35, 32, 190);
+  padding: 30px;
+  text-align: center;
+  font-size: 30px;
+  color: black;
+}
+/* Create two columns/boxes that floats next to each other */
+nav {
+  float: left;
+  width: 30%;
+  height: 300px; /* only for demonstration, should be removed */
+  background: #ccc;
+  padding: 20px;
+}
+/* Style the list inside the menu */
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+article {
+  float: left;
+  padding: 20px;
+  width: 70%;
+  background-color: #f1f1f1;
+  height: 300px; /* only for demonstration, should be removed */
+}
+/* Clear floats after the columns */
+section::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+/* Style the footer */
+footer {
+  background-color: rgb(40, 144, 148);
+  padding: 10px;
+  text-align: center;
+  color: rgb(15, 3, 3);
+}
+
+  }
+}
+</style>
+</head>
+<body>
+
+<h2>GOA</h2>
+<hr>WD-101</hr>
+
+
+
+<header>
+  <h2>    Master Mrunal Bhatkande</h2>
+</header>
+
+<section>
+  <nav>
+    <ul>
+      <li><a href="index.html">About Me</a></li>
+      <li><a href="mart.html">Martial Arts</a></li>
+    
+    </ul>
+  </nav>
+  
+  <article>
+    <h1>Lyrics</h1>
+    <p>
+      <br>
+        
+      </p>  
+        
+  </article>
+</section>
+<footer>
+  <p>Goa Engineering College</p>
+</footer>
+</body>
+</html>
